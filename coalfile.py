5cd52e7070c5a0497acf5db22d08e49b48346b18
Nik Voss <niklas.voss@gmail.com@>
2016-11-30 13:01:56 +0100

    Added versioning.

--- coalfile.py
+++ coalfile.py
@@ -3,13 +3,21 @@ from util import git_clone, download, unzip, default_cmake_build, cp
 from os import path
 
 class SpdlogFile(CoalFile):
-    url = "https://github.com/gabime/spdlog"
+    repo = "https://github.com/gabime/spdlog"
+    url = "https://github.com/gabime/spdlog/archive/v%s.zip"
+    zipfile = "spdlog.zip"
     exports = ["include"]
     def prepare(self):
-        git_clone(self.url, 'master', 'src')
+        if "version" in self.settings:
+            version = self.settings["version"]
+            download(self.url % (version), self.zipfile)
+            unzip(self.zipfile, 'temp')
+            cp('temp/spdlog-%s/' % (version), 'temp/') # TODO: mv would be cleaner
+        else:
+            git_clone(self.repo, 'master', 'temp')
     def build(self):
         pass
     def package(self):
-        cp('src/include', 'include')
+        cp('temp/include', 'include')
     def info(self, generator):
         generator.add_include_dir('include/')
